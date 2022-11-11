068b1d46d79c25ecc0ab55a674c5518dc064cb47
bada3670 <iykl3670@gmail.com>
2022-11-11 17:05:52 +0900

    🎉 시작

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Shareground-Home</title>
+      </Head>
+      <div>
+        <h1>Shareground</h1>
+      </div>
+    </>
+  );
+}
