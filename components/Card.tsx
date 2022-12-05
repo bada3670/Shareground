392d220a312f353a7aa359282f5f1428ccc09722
bada3670 <iykl3670@gmail.com>
2022-12-05 22:14:28 +0900

    하단 참고 💄 Card: 파일 형식에 따라 border 색 다르게 하기 🔥 Card: 불필요한 코드 삭제

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,16 +1,8 @@
-import {
-  getFirestore,
-  collection,
-  query,
-  where,
-  orderBy,
-  limit,
-  getDocs,
-  DocumentData,
-} from 'firebase/firestore';
+import { DocumentData } from 'firebase/firestore';
 import dateNumToStr from 'utils/dateNumToStr';
 import style from 'styles/components/Card.module.scss';
 import { useRouter } from 'next/router';
+import { extAud, extDoc, extImg, extVid } from 'utils/fileExtension';
 
 interface Datum {
   id: string;
@@ -21,15 +13,19 @@ export default function Card({ datum }: { datum: Datum }) {
   const { fileType, title, category, username, date } = datum.info;
   const router = useRouter();
 
-  let borderColor = 'black';
+  let fileTypeForBorder = 'default';
 
-  switch (fileType) {
-    case 'png':
-      borderColor = 'red';
-      break;
-
-    default:
-      break;
+  if (extAud.includes(fileType)) {
+    fileTypeForBorder = 'aud';
+  }
+  if (extDoc.includes(fileType)) {
+    fileTypeForBorder = 'doc';
+  }
+  if (extImg.includes(fileType)) {
+    fileTypeForBorder = 'img';
+  }
+  if (extVid.includes(fileType)) {
+    fileTypeForBorder = 'vid';
   }
 
   const click$article = () => {
@@ -38,7 +34,7 @@ export default function Card({ datum }: { datum: Datum }) {
 
   return (
     <article
-      className={`${style['card']} card-${style[borderColor]}`}
+      className={`${style['card']} ${style[fileTypeForBorder]}`}
       onClick={click$article}
     >
       <div className={style['file-type']}>{fileType ?? '파일 없음'}</div>
